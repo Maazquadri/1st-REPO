80f60f4acc4fa15bc586f906682e207afdb40a1d
Maazquadri <stenomaazquadri2014@gmail.com>
2022-01-21 12:06:38 +0530

    CONCATMETHOD
    concatmethod

---
+++ HTML/geometry/concatmethod.js
@@ -0,0 +1,63 @@
+// const arr1 = [1,2,3];
+// const arr2 = [4,5,6];
+
+// const arr3 = arr1.concat(arr2);     'concat method'
+// console.log(arr3);
+
+
+
+// const newArry=[1,2,3,4,5,6];
+// const popped = newArry. pop();
+
+// console.log(newArry);
+// console.log(popped);
+
+
+
+// const newArry=[1,2,3,4,5,6,7];
+// const pushed=newArry.push(8);
+// console.log(pushed);
+// console.log(newArry);
+
+
+
+// const newArry=[1,2,3,4,5,6,7];
+// const shifted=newArry.shift();
+// console.log(newArry);
+
+
+
+// const newArry=[1,2,3,4,5,6,7];
+// const unshifted=newArry.unshift(8)
+// console.log(newArry);
+
+
+
+// const newArry=[1,2,3,4,5,6,7];
+// const reversed=newArry.reverse();
+// console.log(reversed);
+
+
+
+// const newArry=[1,2,3,4,5,6,7];
+// const result=newArry.forEach((Maaz)=> console.log (Maaz*10));
+// console.log(result);
+
+
+// const newArry=[1,2,3,4,5,6,7];
+// const result=newArry.filter((maaz)=> maaz%2 !==0)
+// .map((maaz)=> maaz*10)
+// .reduce((acc,curr)=>acc+curr);
+// console.log(result);
+
+// const alpha=["a","b","c","d","e","f","g","h","i"];
+// const result= alpha.slice(1,5);
+// console.log(result);
+// console.log(alpha);
+
+const alpha=["a","b","c","d","e","f","g","h","i"];
+const result=alpha.splice(2,0, "ABC", "abc");
+console.log(alpha);
+
+
+
